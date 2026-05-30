218ca8fc091bb2ba057dca576ca253d3f0a38664
Jalix-WebTech <jalixwebtech@gmail.com>
2026-05-30 06:11:34 +0100

    updating date format

--- script.js
+++ script.js
@@ -247,3 +247,9 @@ if (mobileMenuBtn && mobileNav) {
   });
 }
 
+// AUto update year in footer
+const yearEl = document.getElementById("year");
+
+if (yearEl) {
+  yearEl.textContent = new Date().getFullYear();
+}
